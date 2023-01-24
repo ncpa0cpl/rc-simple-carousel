a2eb08545223d58a09c9abe2103e1334b44a844a
ncpa0cpl <sbretner@touchbistro.com>
2023-01-24 16:45:56 +0100

    feat: stop publish script on any error

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,10 +1,21 @@
+#!/usr/bin/env bash
+
+set -o verbose
+set -o xtrace
+set -o errexit
+
 issemver=$(./scripts/check-semver.sh -t "$TAG_NAME")
 currenttag=$(npm pkg get version)
+
+git config user.name github-actions
+git config user.email github-actions@github.com
+
 if [ "$issemver" -eq "1" ]; then
     if ! [ "$currenttag" = "\"$TAG_NAME\"" ]; then
         echo "Branch tag is different than packge.json version. Updating package.json version to $TAG_NAME"
-        npm version --no-git-tag-version "$TAG_NAME"
+        npm version "$TAG_NAME"
+        git push --no-verify
     fi
     echo "Publishing to npm"
-    npm publish && git checkout .
+    npm publish
 fi
